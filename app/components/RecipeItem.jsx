db2f7ae18245bc73924497a76b04365cafa3f58f
zubairrafi <walleeva2018@gmail.com>
2024-06-02 22:40:09 +0600

    Maybe fixed
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- app/components/RecipeItem.jsx
+++ app/components/RecipeItem.jsx
@@ -10,12 +10,12 @@ export default async function RecipeItem() {
     const recipes = await Recipe.find();
 
 
-
+console.log('just checking')
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-8 justify-items-center">
             {recipes.map((recipe) => (
-            <Link href={`/details/${recipe._id}`} key={recipe._id}>
-                <div className="card">
+           <div key={recipe._id}>
+            <Link href={`/details/${recipe._id}`}  className="card">
                     <Image src={recipe.thumbnail} width={500} height={300} className="rounded-md" alt={recipe.name} />
                     <h4 className="my-2">{recipe.name}</h4>
                     <p className="text-gray-500 text-sm mb-2">{recipe.description}</p>
@@ -23,8 +23,8 @@ export default async function RecipeItem() {
                         <span>⭐️ {recipe.rating}</span>
                         <span>By: {recipe.author}</span>
                     </div>
-                </div>
             </Link>
+            </div>
             ))}
         </div>
     );
