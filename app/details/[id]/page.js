ad8df28832cd44fb04d18a85fb482b8aefe622c0
zubairrafi <walleeva2018@gmail.com>
2024-06-03 12:40:55 +0600

    Ekdom basic
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -1,43 +1,9 @@
-import Navbar from '@/app/components/Navbar';
-import Recipe from '@/app/models/Recipies';
-import connectMongo from '@/dbConnect/connectMongo';
-import React from 'react';
-import Image from 'next/image';
-import SocialShare from '../../components/SocialShare'
-import Favourite from '@/app/components/Favourite';
-import User from '@/app/models/User';
+import React from 'react'
 
-export async function generateMetadata({ params, searchParams }, parent) {
-    // read route params
-    const id = params.id
-   
-    // fetch data
-    const recipe = await Recipe.find();
-   
-
-    return {
-      title: recipe[params.id].name,
-      description: recipe[params.id].description
-
-    }
-  }
-
-
-export default async function DetailPage({ params }) {
-    await connectMongo();
-    const recipe = await Recipe.find();
-    const users = await User.find()
-  
-    if (!recipe) {
-        return <p>Recipe not found</p>;
-    }
-
-    return (
-        <>
-            <body>
-                <Navbar />
-
-            </body>
-        </>
-    );
+export default function Checker() {
+  return (
+    <>
+    Chekreer just work first 
+    </>
+  )
 }
